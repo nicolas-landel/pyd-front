1806a3524ed148228f79371c5bc4d235c017bec5
Nicolas <nicolas.landel@hotmail.com>
2025-05-04 15:39:43 +0200

    Manage case string

--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,12 +1,20 @@
 export function flattenResponseData(data) {
+  if (typeof data === 'string') {
+    return "";
+  }
   return Object.entries(data)
     .map(([key, value]) => {
       const messages = Array.isArray(value) ? value.join(', ') : value;
-      return `${capitalizeFirstLetter(key)}: ${messages}`;
+      const truncatedMessage = truncateMessage(messages, 100); // Limit to 100 characters
+      return `${capitalizeFirstLetter(key)}: ${truncatedMessage}`;
     })
     .join('\n');
 }
 
 function capitalizeFirstLetter(string) {
   return string.charAt(0).toUpperCase() + string.slice(1);
+}
+
+function truncateMessage(message, maxLength) {
+  return message.length > maxLength ? `${message.slice(0, maxLength)}...` : message;
 }
