61bb357f88e063f29883866c992c880fe24e2613
Nicolas <nicolas.landel@hotmail.com>
2025-05-10 22:50:18 +0200

    Set default redirection

---
+++ src/pages/[...all].vue
@@ -0,0 +1,12 @@
+<template>
+  <div />
+</template>
+
+<script setup lang="ts">
+import { useRouter } from 'vue-router'
+
+const router = useRouter()
+
+router.replace('/home')
+</script>
+
--- src/pages/home.vue
+++ src/pages/home.vue
@@ -6,6 +6,17 @@
 </template>
 
 <script setup lang="ts">
-import TheLoginForm from '~/components/TheLoginForm.vue';
+// import TheLoginForm from '~/components/TheLoginForm.vue';
+
+import { useRepo } from 'pinia-orm';
+import User from '~/models/User';
+import { useUserStore } from '~/stores/user';
+
+const userStore = useUserStore();
+
+onBeforeMount(() => {
+  userStore
+});
+
 
 </script>
